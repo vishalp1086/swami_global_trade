3e6299a06e28ef27ef2532a9b7641c8cce9643a1
vishalp1086 <patilvishals86@gmail.com>
2026-05-07 16:14:36 +0530

    mailid

--- server/routes/enquiry.js
+++ server/routes/enquiry.js
@@ -15,7 +15,7 @@ router.post("/", async (req, res) => {
 
     await resend.emails.send({
       from: "onboarding@resend.dev", // default testing sender
-      to: "patilvishals86@gmail.com",
+      to: "vinayak@swamiglobaltrade.com",
       subject: "New Enquiry from Website",
 
       html: `
